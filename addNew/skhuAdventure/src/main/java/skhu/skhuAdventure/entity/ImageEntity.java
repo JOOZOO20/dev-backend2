73fd2f08ed2f35196c51e0be7717b4be431d780d
Heeju <62732461+JOOZOO20@users.noreply.github.com>
2024-06-19 20:47:40 +0900

    skhuAdventure 임시 저장
    컴퓨터 옮겨서 테스트하기위해 임시로 올리는 skhuAdventure파일

---
+++ addNew/skhuAdventure/src/main/java/skhu/skhuAdventure/entity/ImageEntity.java
@@ -0,0 +1,23 @@
+package skhu.skhuAdventure.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Setter
+@Getter
+@Entity
+public class ImageEntity {
+
+    // Getters and setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int userId;
+    private int imageNumber;
+    private String imageUri;
+}
